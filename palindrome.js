af08e21ec878743b7bb6006ff0b4efa674e69565
Kuldeep Kumar Baitha <kuldeepkumar83986@gmail.com>
2022-12-04 21:33:29 +0530

    prime and palindrome file added

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+let str = "madam";
+let str1 = "";
+for (let i = str.length - 1; i >= 0; i--) {
+  str1 += srt[i];
+}
+if (str == str1) {
+  console.log("it is palindrome");
+}
+else {
+  console.log("Not palindrome");
+}
